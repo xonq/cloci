c919841de5695cc8e79ece54382ee361a7d161a9
xonq <konkelzach@protonmail.com>
2023-04-06 18:52:12 -0400

    cloci

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open( "README.md", "r" ) as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "bigmcl",
-    version = "0.2b2",
+    name = "Cloci",
+    version = "0.0b1",
     author = "xonq",
     author_email = "konkelzach@protonmail.com",
-    description = "Large scale Markov clustering (MCL) via subgraph extraction",
+    description = "Cloci evolution-based gene cluster detection",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "https://gitlab.com/xonq/bigmcl",
+    url = "https://gitlab.com/xonq/cloci",
     package_dir={"": "./"},
     packages = setuptools.find_packages(),
-    scripts = ["bigmcl/bigmcl.py"],
+    scripts = ["cloci/cloci"], # add more
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
