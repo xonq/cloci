01b661d1780790d88902192063b22402afecd6eb
xonq <konkelzach@protonmail.com>
2023-06-28 18:52:31 -0400

    "0.0.3"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open( "README.md", "r" ) as fh:
 
 setuptools.setup(
     name = "cloci",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "xonq",
     author_email = "konkelzach@protonmail.com",
     description = "Function-agnostic gene cluster detection",
@@ -15,7 +15,7 @@ setuptools.setup(
     package_dir={"": "cloci"},
     packages = setuptools.find_packages( where="cloci" ),
     scripts = ['cloci/lib/output_data.py', 'cloci/lib/evo_conco.py', 'cloci/lib/generate_nulls.py', 'cloci/lib/hgp2hgx.py', 'cloci/lib/hgx2hlgs.py', 'cloci/lib/input_parsing.py', 'cloci/lib/treecalcs.py'],
-    install_requires = ['mycotools', 'numpy', 'scipy', 'graph-tool', 'cogent3'],
+    install_requires = ['mycotools', 'numpy', 'scipy', 'graph-tools', 'cogent3'],
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
