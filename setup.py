daa52fc5742cab3112714352eaf904bdecbe89ec
xonq <konkelzach@protonmail.com>
2023-06-29 10:00:04 -0400

    "0.0.13"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open( "README.md", "r" ) as fh:
 
 setuptools.setup(
     name = "cloci",
-    version = "0.0.12",
+    version = "0.0.13",
     author = "xonq",
     author_email = "konkelzach@protonmail.com",
     description = "Function-agnostic gene cluster detection",
@@ -14,8 +14,8 @@ setuptools.setup(
     url = "https://github.com/xonq/cloci/",
     package_dir={"": "cloci"},
     packages = setuptools.find_packages( where="cloci" ),
-    scripts = ['README.md', 'TODO.md', 'cloci/tools/cloci2stats.py', 'cloci/tools/hlg2biofile.py', 'cloci/tools/hlg2hlg_net.py', 'cloci/tools/cloci2enrich.py', 'cloci/cloci'],
-    install_requires = ['mycotools', 'numpy', 'scipy', 'graph-tools', 'cogent3'
+    scripts = ['README.md', 'TODO.md', 'cloci/tools/cloci2stats.py', 'cloci/tools/hlg2biofile.py', 'cloci/tools/hlg2hlg_net.py', 'cloci/tools/cloci2enrich.py', 'README.md', 'TODO.md', 'cloci/cloci'],
+    install_requires = ['mycotools', 'numpy', 'scipy', 'graph-tools', 'cogent3',
                         'sniffio', 'pydantic', 'PyYAML', 'SQLAlchemy', 'packaging',
                         'pydantic', 'anyio', 'attrs'],
     classifiers = [
